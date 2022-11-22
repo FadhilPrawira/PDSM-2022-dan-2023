316628fae39b7ca99e54d4399f3fb9d568af0666
Fadhil Prawira <fadhilprawira87@gmail.com>
2022-11-22 16:34:44 +0700

    Add files via upload

---
+++ AT89S51-AT89S52/Subpercobaan 7 Alarm Kebakaran/p1_7 fix.asm
@@ -0,0 +1,20 @@
+org 0000h
+	setb P0.1	;input LDR gelap
+	mov P1,#0ffh	;membersihkan led com anode
+
+start:	jnb P0.1,loop 
+	mov P1,#0ffh
+	sjmp start
+loop:	mov P1,#0ffh
+	acall delay
+	mov P1,#000h
+	acall delay
+	sjmp start
+delay:	mov R0,#01h
+delay0:	mov R1,#0f0h
+delay1: mov R2,#0F0h
+delay2:	djnz R2,delay2
+	djnz R1,delay1
+	djnz R0,delay0
+	ret
+end
