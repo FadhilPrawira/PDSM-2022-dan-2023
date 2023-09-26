d4a961240793bedfdf0755c07beb63b3307b1a4e
Fadhil Prawira <fadhilprawira87@gmail.com>
2023-09-26 15:33:28 +0700

    Update Subpercobaan_4_DHT11.ino

--- Arduino/Subpercobaan_4_DHT11/Subpercobaan_4_DHT11.ino
+++ Arduino/Subpercobaan_4_DHT11/Subpercobaan_4_DHT11.ino
@@ -1,37 +1,56 @@
-/*********
-  Rui Santos
-  Complete project details at http://randomnerdtutorials.com  
-  Based on the Dallas Temperature Library example
-*********/
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include "DHT.h"
 
-// Data wire is conntec to the Arduino digital pin 4
-#define ONE_WIRE_BUS 4
+#define DHTPIN 2     // what pin we're connected to
 
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(ONE_WIRE_BUS);
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11 
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Pass our oneWire reference to Dallas Temperature sensor 
-DallasTemperature sensors(&oneWire);
+// Initialize DHT sensor for normal 16mhz Arduino
+DHT dht(DHTPIN, DHTTYPE);
 
-void setup(void)
-{
-  // Start serial communication for debugging purposes
-  Serial.begin(9600);
-  // Start up the library
-  sensors.begin();
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("DHTxx test!");
+ 
+  dht.begin();
 }
 
-void loop(void){ 
-  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
-  sensors.requestTemperatures(); 
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit
+  float f = dht.readTemperature(true);
   
-  Serial.print("Celsius temperature: ");
-  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  Serial.print(sensors.getTempCByIndex(0)); 
-  Serial.print(" - Fahrenheit temperature: ");
-  Serial.println(sensors.getTempFByIndex(0));
-  delay(1000);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index
+  // Must send in temp in Fahrenheit!
+  float hi = dht.computeHeatIndex(f, h);
+
+  Serial.print("Humidity: "); 
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hi);
+  Serial.println(" *F");
 }
