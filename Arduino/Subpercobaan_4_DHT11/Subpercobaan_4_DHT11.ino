8c55f3abdc3cc7ec7946803fd3b7bbe08c9a4155
Fadhil Prawira <fadhilprawira87@gmail.com>
2023-09-26 15:37:44 +0700

    Update Subpercobaan_4_DHT11.ino

--- Arduino/Subpercobaan_4_DHT11/Subpercobaan_4_DHT11.ino
+++ Arduino/Subpercobaan_4_DHT11/Subpercobaan_4_DHT11.ino
@@ -1,56 +1,27 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
+#include <dht.h>        // Include library
+#define outPin 8        // Defines pin number to which the sensor is connected
 
-#include "DHT.h"
-
-#define DHTPIN 2     // what pin we're connected to
-
-// Uncomment whatever type you're using!
-#define DHTTYPE DHT11   // DHT 11 
-//#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Initialize DHT sensor for normal 16mhz Arduino
-DHT dht(DHTPIN, DHTTYPE);
+dht DHT;                // Creates a DHT object
 
 void setup() {
-  Serial.begin(9600); 
-  Serial.println("DHTxx test!");
- 
-  dht.begin();
+	Serial.begin(9600);
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);
+	int readData = DHT.read11(outPin);
 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit
-  float f = dht.readTemperature(true);
-  
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
+	float t = DHT.temperature;        // Read temperature
+	float h = DHT.humidity;           // Read humidity
 
-  // Compute heat index
-  // Must send in temp in Fahrenheit!
-  float hi = dht.computeHeatIndex(f, h);
+	Serial.print("Temperature = ");
+	Serial.print(t);
+	Serial.print("°C | ");
+	Serial.print((t*9.0)/5.0+32.0);        // Convert celsius to fahrenheit
+	Serial.println("°F ");
+	Serial.print("Humidity = ");
+	Serial.print(h);
+	Serial.println("% ");
+	Serial.println("");
 
-  Serial.print("Humidity: "); 
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: "); 
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hi);
-  Serial.println(" *F");
+	delay(2000); // wait two seconds
 }
